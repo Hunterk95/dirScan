c051146e81bae72a55d0e5daf74757fdc91adb46
Hunterk95 <hunterk95@mail.ru>
2021-06-26 14:19:02 +0300

    test from task example

--- test/scanner/FileScannerTest.java
+++ test/scanner/FileScannerTest.java
@@ -11,12 +11,14 @@ import java.util.List;
 import java.util.Set;
 
 class FileScannerTest {
-    private List<Path> paths = new ArrayList<>();
-    private Set<Path> ignorePaths = new HashSet<>();
+    private List<Path> paths;
+    private Set<Path> ignorePaths;
 
     @Test
     void isOrderConst() {
+        paths = new ArrayList<>();
         paths.add(new File("/usr").toPath());
+        ignorePaths = new HashSet<>();
         ignorePaths.add(new File("/usr/share").toPath());
         ignorePaths.add(new File("/usr/lib").toPath());
         ignorePaths.add(new File("/usr/bin").toPath());
@@ -27,5 +29,21 @@ class FileScannerTest {
         scanner.scan(4);
         String scan2 = scanner.toString();
         Assertions.assertEquals(scan1, scan2);
+    }
+
+    @Test
+    void noNeedToScan() {
+        String[] args = {"\\\\epbyminsd0235\\Video Materials",
+                "\\\\EPUALVISA0002.kyiv.com\\Workflow\\ORG\\Employees\\Special",
+                "\\\\EPUALVISA0002.kyiv.com\\Workflow\\ORG\\Employees\\Lviv",
+                "-",
+                "\\\\epbyminsd0235\\Video Materials",
+                "\\\\EPUALVISA0002.kyiv.com\\Workflow\\ORG\\Employees\\Special",
+                "\\\\EPUALVISA0002.kyiv.com\\Workflow\\ORG\\Employees\\Lviv"};
+        CommandLineArgParser parser = new CommandLineArgParser(args);
+        parser.parse();
+        FileScanner scanner = new FileScanner(parser.getPaths(), parser.getIgnorePaths());
+        scanner.scan(parser.getNumOfThreads());
+        Assertions.assertEquals(scanner.toString(), "");
     }
 }
